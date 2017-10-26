c879f625640fa777681e077b352e196be236a18a
Jonas Wirth <jonas-wirth@gmx.ch>
2017-10-26 22:47:32 +0200

    Refactoring

--- GetConnections.js
+++ GetConnections.js
@@ -8,12 +8,86 @@ var instantMilliSeconds = 0;
 const apiBaseUrl = "http://transport.opendata.ch/v1/connections";
 const startPosition = "Wallisellen_Neugut";
 
+/*Destination and destinationDisplayNames must be in the same order*/
+const destinations = ["zurich_Flughafen_Fracht","zurich_HB","zurich_ETH","zurich_ETH_Hoenggerberg"];
+const destinationDisplayNames = ["Zürich Flughafen, Fracht", "Zürich HB", "Zürich ZH, ETH/Universitätsspital" , "Zürich, ETH Hönggerberg"];
+
+
 function getUrl(destination){
   var date = new Date();
   return apiBaseUrl + "?time=" + date.getHours() + ":" + date.getMinutes() + "&from=" + startPosition + "&to=" + destination;
 }
 
+function updateDestination(destination, departure, minutesLeft, duration, arrival, vehicle){
+  /*Map display name to index*/
+  var index = parseInt(destinationDisplayNames.indexOf(destination)) + 1;
+
+  document.getElementById("departureTime"+index).innerHTML = formatTime(departure);
+  document.getElementById("departMin"+index).innerHTML = minutesLeft + " min";
+  document.getElementById("duration"+index).innerHTML = duration + " min";
+  document.getElementById("type"+index).innerHTML = vehicle;
+  document.getElementById("arrivalTime"+index).innerHTML = formatTime(arrival);
+
+  if(minutesLeft <= 5){
+    document.getElementById("departMin"+index).style.color = "red";
+  }else{
+    document.getElementById("departMin"+index).style.color = "black";
+  }
+}
+
+function formatTime(date){
+  var hours;
+  var minutes;
+  if (date.getHours() / 10 < 1) {
+      hours = '0' + date.getHours();
+  }
+  else hours = date.getHours();
+  if (date.getMinutes() / 10 < 1) {
+      minutes = '0' + date.getMinutes();
+  }
+  else minutes = date.getMinutes();
+  return hours + ':' + minutes;
+}
+
+function getVehicle(vehicle){
+  return vehicle.category + vehicle.number + " > " + vehicle.to;
+}
+
+/*Get the next connection*/
+function getConnection(connections){
+  var currentTime = new Date();
+  for(var i in connections){
+    if(currentTime - new Date(connections[i].from.departure) < 0)
+      return connections[i];
+  }
+}
+
 var getConnections = function () {
+    for(var i in destinations){
+      axios.get(getUrl(destinations[i]))
+      .then(function(response){
+        console.log(response);
+          var currentTime = new Date();
+          var connection = getConnection(response.data.connections);
+
+          var departureTime = new Date(connection.from.departure);
+          var minutesLeft = Math.round((((departureTime - currentTime) % 86400000) % 3600000) / 60000);
+          var arrivalDate = new Date(connection.to.arrival);
+          var duration = Math.round((((arrivalDate - departureTime) % 86400000) % 3600000) / 60000);
+
+          var vehicle = getVehicle(connection.sections[0].journey);
+          var destination = response.data.to.name;
+
+          //console.log(departureTime + '\n' + diffMins + '\n' + arrivalDate + '\n' + durration);
+          updateDestination(destination, departureTime, minutesLeft, duration, arrivalDate, vehicle);
+      })
+      .catch(function(error){
+        console.log(error);
+      })
+    }
+
+
+
     var HttpClient = function () {
         this.get = function (url, callBack) {
             var httpRequest = new XMLHttpRequest();
@@ -27,11 +101,7 @@ var getConnections = function () {
         }
     }
 
-    var date = new Date();
-
-    var hour = date.getHours();
-    var minute = date.getMinutes();
-    instantMilliSeconds = date.getMilliseconds();
+    instantMilliSeconds = new Date().getMilliseconds();
 
     var flugHafenConnection = new HttpClient();
     flugHafenConnection.get(getUrl("zurich_Flughafen_Fracht"), function (response) {
@@ -53,6 +123,7 @@ var getConnections = function () {
         var arrivalDate = new Date(focusedConnection.to.arrival);
         var durration = Math.round((((arrivalDate - departureTime) % 86400000) % 3600000) / 60000);
 
+          /**/
 
         /***************************************************************************************************/
         var departureHours;
@@ -96,7 +167,7 @@ var getConnections = function () {
             var colorChanger = document.getElementById("departMin1");
             colorChanger.style.color = "red";
         }
-        document.getElementById("duration1").innerHTML = durration + " min";
+        /*document.getElementById("duration1").innerHTML = durration + " min";*/
         document.getElementById("type1").innerHTML = vehicleCategory + "" + vehicleNumber + " > " + direction;
         document.getElementById("arrivalTime1").innerHTML = arrivalTime;
 
@@ -166,7 +237,7 @@ var getConnections = function () {
             var colorChanger = document.getElementById("departMin2");
             colorChanger.style.color = "red";
         }
-        document.getElementById("duration2").innerHTML = durration + " min";
+        /*document.getElementById("duration2").innerHTML = durration + " min";*/
         document.getElementById("type2").innerHTML = vehicleCategory + "" + vehicleNumber + " > " + direction;
         document.getElementById("arrivalTime2").innerHTML = arrivalTime;
 
@@ -236,7 +307,7 @@ var getConnections = function () {
             var colorChanger = document.getElementById("departMin3");
             colorChanger.style.color = "red";
         }
-        document.getElementById("duration3").innerHTML = durration + " min";
+        /*document.getElementById("duration3").innerHTML = durration + " min";*/
         document.getElementById("type3").innerHTML = vehicleCategory + "" + vehicleNumber + " > " + direction;
         document.getElementById("arrivalTime3").innerHTML = arrivalTime;
 
@@ -306,7 +377,7 @@ var getConnections = function () {
             var colorChanger = document.getElementById("departMin4");
             colorChanger.style.color = "red";
         }
-        document.getElementById("duration4").innerHTML = durration + " min";
+        /*document.getElementById("duration4").innerHTML = durration + " min";*/
         document.getElementById("type4").innerHTML = vehicleCategory + "" + vehicleNumber + " > " + direction;
         document.getElementById("arrivalTime4").innerHTML = arrivalTime;
 
